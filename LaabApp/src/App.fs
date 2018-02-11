d33d406e14dbfb1f496fea8277de0c5a68b8c81f
Sabrina Issa <sztobo@hotmail.fr>
2018-02-11 17:58:58 +0100

    Reloading url OK

--- LaabApp/src/App.fs
+++ LaabApp/src/App.fs
@@ -42,6 +42,11 @@ let makeVisible (l: NodeListOf<Element>) =
     for i in 0.0 .. (l.length - 1.0) do
         JustLazy?registerLazyLoad(l.item(i)) |> ignore
 
+let spotted = document.URL.Split([|'#'|])
+let firstUrl = match spotted.Length > 1 with
+               | true -> "#" + spotted.[1]
+               | false -> "firstContent"         
+
 let rec fluff (el: HTMLElement) url target origin =
     let curactive = document.querySelector(origin + ".active")
     try
@@ -57,7 +62,11 @@ let rec fluff (el: HTMLElement) url target origin =
         | null -> printfn "Got null with document.getElementById(%s)" url
         | _ -> newVis.hidden <- false        
         if System.String.Equals(el.id,"first") then
+            printfn "first found"
             fluff (document.getElementById("firstContentClick")) "firstContent" "LoadMe" "a.pageFetcher" |> ignore
+            // else let truc = document.querySelector(sprintf @"a[href=""%s""" firstUrl) :?> HTMLElement
+            //      printfn "truc : %s" (truc.getAttribute("href"))
+            //      fluff truc firstUrl "LoadMe" "a.pageFetcher" |> ignore
         else
             makeVisible(newVis.querySelectorAll(".justlazy-spinner"))
         with
@@ -65,14 +74,11 @@ let rec fluff (el: HTMLElement) url target origin =
     el.classList.add("active")
     box true
 
+
 let rec toload target origin =
     let loadme = document.getElementById(target)
     let links = document.querySelectorAll(origin)
     let l = links.length
-    let reparseFun =
-        match target with
-        | "content" -> (fun _ -> toload "LoadMe" "a.pageFetcher")
-        | _ -> ignore
     for i in 0.0 .. (l-1.0) do
         let el = links.item(i) :?> HTMLElement
         let url = 
@@ -80,16 +86,24 @@ let rec toload target origin =
             | "#/" -> el.getAttribute("href")
             | _ -> "#/" + el.getAttribute("href")
         el.setAttribute("href",url)
-        let spotted = document.URL.Split([|'#'|])
-        
-        let hidden = not (System.String.Equals(url,"#/Content.html"))
+        let hidden = match url with
+                     | "#/Content.html" when (firstUrl.Substring(0,10).Equals("#/content-")) || (firstUrl.Equals("firstContent")) -> false  
+                     | txt when target.Equals("content") && (txt.Equals(firstUrl)) -> false
+                     | txt when target.Equals("LoadMe") && (txt.Equals(firstUrl)) -> (if not (firstUrl.Equals("firstContent")) then document.getElementById("firstContent").hidden <- true); false
+                     | _ -> true
         let postFirstContent = match url with
-                                | "#/Content.html" ->  (fun _ -> makeVisible(document.getElementById("firstContent").querySelectorAll(".justlazy-spinner")))
+                                | "#/Content.html" when firstUrl.Equals("firstContent") || (firstUrl.Equals("#/Content.html")) -> (fun _ -> printfn "make one"; makeVisible(document.getElementById("firstContent").querySelectorAll(".justlazy-spinner")))
                                 | _ -> ignore
+        let reparseFun =
+            match target with
+            | "content" when url.Equals("#/Content.html") -> (fun _ -> toload "LoadMe" "a.pageFetcher")
+            | _ when url.Equals(firstUrl) -> (fun _ -> makeVisible(document.getElementById(firstUrl).querySelectorAll(".justlazy-spinner")))
+            | _ -> ignore
         fetchMy url loadme reparseFun postFirstContent hidden |> Async.AwaitPromise |> Async.StartImmediate
         el.onclick <- (fun _ -> fluff el url target origin)
 
 let init() =
+    printfn "first : %s" firstUrl
     toload "content" "nav a"
 
 ready init
