fc0398f2aeef48e369923d8149b41b45d59d6c04
François-David Collin <fradav@gmail.com>
2018-02-17 12:30:14 +0100

    with elmish cmd

--- LaabApp/src/App.fs
+++ LaabApp/src/App.fs
@@ -23,7 +23,7 @@ let fetchMy (url:string) (loadme:Element) post hidden =
         let! response = Fetch.fetch (url.Substring(2)) [] 
         let! body = response.text()
         let mydiv = document.createElement_div()
-        mydiv.id <- url
+        mydiv.id <- url.Split([|'#'|]).[1]
         mydiv.hidden <- hidden
         mydiv.innerHTML <- body
         loadme.appendChild(mydiv) |> ignore
@@ -41,36 +41,34 @@ let makeVisible (l: NodeListOf<Element>) =
     for i in 0.0 .. (l.length - 1.0) do
         JustLazy?registerLazyLoad(l.item(i)) |> ignore
 
-let spotted = document.URL.Split([|'#'|])
-let firstUrl = match spotted.Length > 1 with
-               | true -> "#" + spotted.[1]
-               | false -> "firstContent"         
 
-let rec fluff (el: HTMLElement) url target origin =
-    let curactive = document.querySelector(origin + ".active")
+let rec fluff (url: string) target origin =
+    printfn "fluff : %A %A %A" url target origin
     try
+        let idurl = url.Split([|'#'|]).[1]
+        let el = document.getElementById(idurl)
+        match el with
+        | null -> printfn "Got null with document.getElementById(%s)" idurl
+        | _ -> el.hidden <- false        
+               el.classList.add("active")
+
+        let curactive = document.querySelector(origin + ".active")
         match curactive with
         | null -> ()
         | _ -> curactive.classList.remove("active")
+
         let old = document.querySelectorAll("#" + target + "> div") |> findVisible
         match Seq.isEmpty old with
         | true -> ()
         | false -> (old |> Seq.head).hidden <- true
-        let newVis = document.getElementById(url)
-        match newVis with
-        | null -> printfn "Got null with document.getElementById(%s)" url
-        | _ -> newVis.hidden <- false        
         if System.String.Equals(el.id,"first") then
-            fluff (document.getElementById("firstContentClick")) "firstContent" "LoadMe" "a.pageFetcher" |> ignore
+                    fluff "/firstContent" "LoadMe" "a.pageFetcher" |> ignore
         else
-            makeVisible(newVis.querySelectorAll(".justlazy-spinner"))
+            makeVisible(el.querySelectorAll(".justlazy-spinner"))
+
         with
             | ex -> printfn "%s" ex.Message
-    el.classList.add("active")
-    box true
 
-let selectHref href = 
-    document.querySelector(sprintf @"a[href=""%s""" href) :?> HTMLElement
 
 let rec toload target origin =
     let loadme = document.getElementById(target)
@@ -78,15 +76,13 @@ let rec toload target origin =
     let l = links.length
     for i in 0.0 .. (l-1.0) do
         let el = links.item(i) :?> HTMLElement
-        let url = 
-            match (el.getAttribute("href")).Substring(0,2) with
-            | "#/" -> el.getAttribute("href")
-            | _ -> "#/" + el.getAttribute("href")
+        let id = el.getAttribute("href")
+        let url = "#/" + id
         el.setAttribute("href",url)
-        let hidden = not (System.String.Equals(url,"#/Content.html"))
+        let hidden = not (System.String.Equals(id,"Content.html"))
         let reparseFun =
             match target with
-            | "content" when url.Equals("#/Content.html") || (firstUrl.Equals("firstContent")) -> (fun _ -> toload "LoadMe" "a.pageFetcher")
+            | "content" when id.Equals("Content.html") -> (fun _ -> toload "LoadMe" "a.pageFetcher")
             | _ -> ignore
         fetchMy url loadme reparseFun hidden |> Async.AwaitPromise |> Async.StartImmediate
 
@@ -108,31 +104,33 @@ let route : Parser<Route->_,_> =
           map Content (top </> custom "content" contentParse)
           map Nav (top </> str)]
 
-let mapRoute result = 
+open Elmish
+
+
+let mapRoute (result:Option<Route>) =
     let mapRes = 
         match result with
         | Some s -> s
         | None -> Content "firstContent"
-    match mapRes with
-    | Content s -> fluff (selectHref "#/Content.html") "#/Content.html" "content" "nav a" |> ignore
-                   fluff (selectHref ("#/" + s)) ("#/" + s) "LoadMe" "a.pageFetcher" |> ignore
-    | Nav s -> fluff (selectHref ("#/" + s)) ("#/" + s) "content" "nav a" |> ignore                 
-    mapRes
+    let mapFun = match mapRes with
+                 | Content s -> (fun _ -> promise { fluff "#/Content.html" "content" "nav a" |> ignore
+                                                    fluff ("#/" + s) "LoadMe" "a.pageFetcher" |> ignore })
+                 | Nav s -> (fun _ -> promise { fluff ("#/" + s) "content" "nav a" |> ignore })         
+    mapRes, (Cmd.ofPromise mapFun () id ignore)
     
-open Elmish
-
 let urlUpdate (result:Option<Route>) _ =
-  printfn "urlUpdate : %A" result
-  mapRoute result,[]
-  
+    printfn "urlUpdate : %A" result
+    mapRoute result
+    
 let init (result:Option<Route>) =
     ready (fun _ -> toload "content" "nav a")
     printfn "init : %A" result
-    mapRoute result,Cmd.none 
+    mapRoute result
 
-let update (msg:Option<Route>) m =
+let update _ m = 
+    printfn "update : %A" m
     m, Cmd.none
 
-Program.mkProgram init update (fun _ _ -> ())
+Program.mkProgram init update (fun m _ -> printfn "view : %A" m)
 |> Program.toNavigable (parseHash route) urlUpdate
 |> Program.run
